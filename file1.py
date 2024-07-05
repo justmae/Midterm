96a3e299c5640693ba9b9aafebc89bdbdaa313fd
justmae <maymay122602@gmail.com>
2024-07-05 11:41:28 +0800

    update file1.py

--- file1.py
+++ file1.py
@@ -5,3 +5,4 @@ sequence.append(sequence[-1] + sequence[-2])
 return sequence
 result = fibonacci_sequence(10)
 print(f"Fibonacci sequence up to 10 numbers: {result}")
+print("(⁠╯⁠ರ⁠ ⁠~⁠ ⁠ರ⁠)⁠╯⁠︵⁠ ⁠┻⁠━⁠┻")
